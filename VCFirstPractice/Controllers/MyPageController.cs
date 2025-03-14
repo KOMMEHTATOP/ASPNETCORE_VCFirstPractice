371772628834d62afc26f5b4102eadef4dbd969d
Admin <123456+KOMMEHTATOP@users.noreply.github.com>
2025-03-14 19:01:01 +0500

    Перехожу в райдер. Там хотрелоуд лучше работает.

--- VCFirstPractice/Controllers/MyPageController.cs
+++ VCFirstPractice/Controllers/MyPageController.cs
@@ -1,11 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VCFirstPractice.Data;
+using VCFirstPractice.Models;
 
 namespace VCFirstPractice.Controllers
 {
     public class MyPageController : Controller
     {
+
+        private readonly ApplicationDbContext _db;
+
+        public MyPageController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            IEnumerable<BooksModel> objList = _db.Books;
+            return View(objList);
+        }        
+        
+        //GET
+        public IActionResult AddNewBook()
+        {
+            string title = "";
+            string author = "";
+            double price;
             return View();
         }
     }
